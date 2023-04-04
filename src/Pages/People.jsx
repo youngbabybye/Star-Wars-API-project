80da10a4a658cc345613018a9473194dfcb20d8a
Viktor <zeninoid@list.ru>
2023-04-04 16:53:20 +0400

    CringeUpdate

--- src/Pages/People.jsx
+++ src/Pages/People.jsx
@@ -1,30 +1,48 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {Pagination, Stack, Container} from "@mui/material"
+const Base_URL = "https://swapi.dev/api/people/?page=";
 
 const People = () => {
     const [people, setPeople] = useState([]);
+    const [page, setPage] = useState(1);
+    const [pageQty, setPageQty] = useState(0);
 
     useEffect(() => {
-        fetch(`https://swapi.dev/api/people/14/`)
+        fetch(Base_URL + `${page}`)
         .then((res) => res.json())
-        .then((data => setPeople(data)))
+        .then((data) => {
+            console.log(data)
+            setPeople(data.results)
+            setPageQty(data.count)  
+        })
         .catch((err) => {
             console.warn(err);
             alert("Ошибка при получении данных");
         })
-    }, []);
+    }, [page]);
     return (
         <div>
             <h1>People</h1>
             {
                 people && people.map(person =>(
-                    <Link key={person.id} to={`/people/${person.id}`}>
+                    <Link key={person.name} to={`/people/${person.url.slice(29)}`}>
                         <li>
                             {person.name}
                         </li>
                     </Link>
+                
                 ))
             }
+            <Container>
+                <Stack spacing={2}>
+                    {
+                        !!pageQty && (
+                            <Pagination count={((pageQty / 10) + 1) - ((pageQty % 10) * 0.1) } page={page} onChange={(_, num) => setPage(num)}/>
+                        )
+                    }
+                </Stack>
+            </Container>
         </div>
     )
 }
