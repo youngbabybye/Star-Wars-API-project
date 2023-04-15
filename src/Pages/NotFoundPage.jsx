fa8642bc46dd7478a80edb2572c7a2cffb6778a8
Viktor <zeninoid@list.ru>
2023-04-15 17:29:08 +0400

    notfoundupdate

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,10 +1,22 @@
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>404: Not Found</h1>
+        <div className="container">
+            <div
+                className="error_div"
+                style={{
+                    margin: "70px auto auto",
+
+                    minHeight: "70px",
+                    display: `flex`,
+                    flexDirection: `column`,
+                    alignItems: `center`,
+                    justifyContent: "center",
+                }}
+            >
+                <h1 className="error404">404: Not Found</h1>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-
-export {NotFoundPage}
+export { NotFoundPage };
